7ad4b49896a548c5ab04e387fe26a842ec76dd1b
Samy <samtouddimte@gmail.com>
2026-05-31 15:09:16 +0200

    Ajout du cv

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -28,7 +28,7 @@ export const aboutMe: AboutMe = {
   imageUrl: "",
   githubUsername: "samy-touddimte",
   linkedinUsername: "samytouddimte",
-  cvUrl: "/cv.pdf",
+  cvUrl: "https://github.com/samy-touddimte/cv/raw/main/cv.pdf",
   funDescription: "",
   secretDescription: "Merci de votre visite. Si mon profil retient votre attention, n'hésitez pas à me contacter.",
   altName: "ÉTUDIANT AU PROFIL SCIENTIFIQUE ET INFORMATIQUE",
