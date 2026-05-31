b7c9e7f7770103319301c1cbdd2b3637ac58e0c6
Samy <samtouddimte@gmail.com>
2026-05-31 18:04:31 +0200

    Lien du portfolio

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -13,7 +13,7 @@ export const portfolioData: Portfolio[] = [
     description:
       "Projet full-stack développé en autonomie complète, qui permet un suivi nutritionnel et métabolique adapté à chaque personne et à leurs progressions.",
     technologies: ["Java 21", "Spring Boot 3", "React 18", "PostgreSQL", "Docker", "JWT", "GitHub Actions", "Flyway", "JUnit 5"],
-    projectUrl: "https://macro-track-iota.vercel.app/dashboard",
+    projectUrl: "https://project-macrotrack.vercel.app",
     codeUrl: "https://github.com/samy-touddimte/MacroTrack",
     imageUrl: "/macrotrack.png",
   },
